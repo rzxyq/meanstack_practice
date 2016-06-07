af3412166af5ad3edb59a2f63c4a40e5d32691be
Ruoyan Qin <rq25@cornell.edu>
2016-06-06 23:26:06 -0400

    Try to refactor controllers into another file.

--- public/app.js
+++ public/app.js
@@ -1,8 +1,19 @@
 /**
  * Created by ruoyanqin on 6/6/16.
  */
-//1st way to do it
 var msw = angular.module("meanstackwalkthrough", []);
+msw.config(function($routeProvider){
+    $routeProvider.when('/', {
+        controller: 'SimpleController',
+        templateUrl1: 'View1.html'
+    }).when('partial2', {
+        controller: 'SimpleController',
+        templateUrl1: 'View2.html'
+    }).otherwise({
+        redirectTo: '/'
+    });
+})
+//1st way to do it
 msw.controller("AppCtrl", function($http){
     $scope.people = [{name:'dave',city:'NY'},
         {name:'alice', city:'Boston'},
@@ -14,14 +25,18 @@ angular.module('meanstackwalkthrough', []).controller('SimpleController', functi
     $scope.people = [{name:'dave',city:'NY'},
         {name:'alice', city:'Boston'},
         {name: 'amy', city:'portland'}];
+    $scope.addCustomer = function() {
+        $scope.customers.push({ name: $scope.newCustomer.name, city: $scope.newCustomer.city});
+    }
 });
 
 // //3rd way
 // var myapp = angular.module("meanstackwalkthrough", []);
-// var controllers = {}
+// var controllers = {};
 // controllers.SimpleController2 = function($scope) {
 //     $scope.people = [{name:'dave',city:'NY'},
 //         {name:'alice', city:'Boston'},
 //         {name: 'amy', city:'portland'}];
 // };
 // myapp.controller(controllers);
+
