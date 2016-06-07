692e0dc2a660fcbb5d045738dc4d725cef6e4a72
Ruoyan Qin <rq25@cornell.edu>
2016-06-07 08:56:41 -0400

    Added ng-route. Still haven't gotten ngview working

--- public/app.js
+++ public/app.js
@@ -1,33 +1,39 @@
 /**
  * Created by ruoyanqin on 6/6/16.
  */
-var msw = angular.module("meanstackwalkthrough", []);
-msw.config(function($routeProvider){
+var msw = angular.module("meanstackwalkthrough", ['ngRoute']);
+msw.config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {
     $routeProvider.when('/', {
         controller: 'SimpleController',
-        templateUrl1: 'View1.html'
-    }).when('partial2', {
+        templateUrl: 'View1.html'
+    }).when('/partial2', {
         controller: 'SimpleController',
-        templateUrl1: 'View2.html'
+        templateUrl: 'View2.html'
     }).otherwise({
         redirectTo: '/'
     });
+        $locationProvider.html5Mode(true);
+}])
+    .factory('simpleFactory', function() {
+        var factory = {};
+        var customers = ['alice', 'bob'];
+        factory.getCustomers = function() {
+            return customers;
+        }
+        return factory;
+    })
+    .controller("AppCtrl", function($scope, simpleFactory){
+    $scope.customers = simpleFactory.getCustomers();
 })
-//1st way to do it
-msw.controller("AppCtrl", function($http){
-    $scope.people = [{name:'dave',city:'NY'},
-        {name:'alice', city:'Boston'},
-        {name: 'amy', city:'portland'}];
-})
-
-//2nd way to do ti
-angular.module('meanstackwalkthrough', []).controller('SimpleController', function($scope) {
+    .controller('SimpleController', function($scope) {
     $scope.people = [{name:'dave',city:'NY'},
         {name:'alice', city:'Boston'},
         {name: 'amy', city:'portland'}];
     $scope.addCustomer = function() {
-        $scope.customers.push({ name: $scope.newCustomer.name, city: $scope.newCustomer.city});
-    }
+        $scope.people.push({ name: $scope.newCustomer.name,
+                                city: $scope.newCustomer.city});
+    };
 });
 
 // //3rd way
@@ -39,4 +45,3 @@ angular.module('meanstackwalkthrough', []).controller('SimpleController', functi
 //         {name: 'amy', city:'portland'}];
 // };
 // myapp.controller(controllers);
-
