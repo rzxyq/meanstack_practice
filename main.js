6bad2ff3146e418ba1b4ccfa0ca1b96b1687509b
Ruoyan Qin <rq25@cornell.edu>
2016-06-15 10:10:50 -0400

    Added mongolab settings

--- main.js
+++ main.js
@@ -15,7 +15,21 @@ var users = require('./routes/users');
 var app = express();
 
 //db
-mongoose.connect('mongodb://localhost/meanstackwalkthrough');
+// Here we find an appropriate database to connect to, defaulting to
+// localhost if we don't find one.
+var uristring =
+    process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL ||
+    'mongodb://localhost/HelloMongoose';
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + uristring);
+  }
+});
 var User = require('./model/user');
 var Product = require('./model/product');
 var Admin = require('./model/admin');
