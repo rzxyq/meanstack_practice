60371016e543dab17d84d95c186067272b2f448e
Ruoyan Qin <rq25@cornell.edu>
2016-07-29 14:15:17 -0400

    setup karma

---
+++ tests/test-frontend.js
@@ -0,0 +1,25 @@
+describe('AngularJS test suite', function() {
+    // Product.collection.drop();
+    // beforeEach(function(done){
+    //     var newBlob = new Product({
+    //         name: 'pineapple'
+    //     });
+    //     newBlob.save(function(err) {
+    //     });
+    //     var newBlob2 = new Product({
+    //         name: 'banana'
+    //     });
+    //     newBlob2.save(function(err) {
+    //         done();
+    //     });
+    // });
+    // afterEach(function(done){
+    //     Product.collection.drop();
+    //     done();
+    // });
+
+    it('should have tests', function() {
+        expect(true).toBe(true);
+    });
+
+});
