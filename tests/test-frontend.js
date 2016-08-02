5f7cc9c1479d0837ee15d7ccc7ee6c1bc1dcb240
Ruoyan Qin <rq25@cornell.edu>
2016-08-02 12:37:33 -0400

    Added service test

--- tests/test-frontend.js
+++ tests/test-frontend.js
@@ -1,5 +1,33 @@
+describe('Friends Unit Tests', function(){
+    var Friends;
+    beforeEach(module('meanstackwalkthrough.services'));
+
+    beforeEach(inject(function (_simpleFactory_) {
+        simpleFactory = _simpleFactory_;
+    }));
+
+    it('can get an instance of my factory', inject(function(simpleFactory) {
+        expect(simpleFactory).toBeDefined();
+    }));
+
+    it('has 5 chats', inject(function(simpleFactory) {
+        expect(simpleFactory.getCustomers().length).toEqual(3);
+    }));
+
+    // it('has Max as friend with id 1', inject(function(Friends) {
+    //     var oneFriend = {
+    //         id: 1,
+    //         name: 'Max Lynx',
+    //         notes: 'Odd obsession with everything',
+    //         face: 'https://avatars3.githubusercontent.com/u/11214?v=3&amp;s=460'
+    //     };
+    //
+    //     expect(Friends.get(1).name).toEqual(oneFriend.name);
+    // }));
+});
+
 describe('SimpleController', function() {
-    beforeEach(module('meanstackwalkthrough'));
+    beforeEach(module('meanstackwalkthrough.controllers'));
     var ctrl, scope;
     beforeEach(inject(function($controller, $rootScope){
         scope = $rootScope.$new(); //reset rootscope
